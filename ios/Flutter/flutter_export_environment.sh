2f5a0e88ea3461b4f55439c950671ee355caf8c7
jessie27hu <jessie27hu@163.com>
2023-07-25 13:37:08 +0800

    提交

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\flutter\flutter_windows_3.10.2-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=F:\FlutterProject\my_loading\my_loading"
+export "FLUTTER_ROOT=F:\flutter_sdk\flutter"
+export "FLUTTER_APPLICATION_PATH=F:\FlutterProject\my_loading"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.0.1"
-export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1.0.0"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
